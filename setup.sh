a97979c33227a1254ae9c41a727aca9e18001d51
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2023-09-25 02:05:46 +0700

    Create setup.sh
    README.md

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/danielmiessler/SecLists.git
+git clone https://github.com/praetorian-inc/Hob0Rules.git
+git clone https://github.com/gwen001/github-subdomains.git
+git clone https://github.com/tiyeume25112004/SubmainFuzz.git
+sudo apt install ffuf
+sudo apt install john
+sudo apt install exiftool
+sudo apt install nmap
+
